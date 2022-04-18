147b7ad001b2726bb1eeaf77b4f74320075c6060
Bastian Onetto <bastian.onetto@usach.cl>
2022-04-18 17:00:15 -0400

    primer release

---
+++ src/main/java/com/example/demo/MensajeController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MensajeController {
+
+    @GetMapping("/mensaje")
+    public String Mensaje(){
+        return ("Aplicacion web muy simple");
+    }
+    
+}
